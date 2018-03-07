475c23f427527366966c630ecaa9df06c1bfa45d
Ju111 <SchneiderJu@gmx.net>
2018-03-07 10:02:09 +0100

    design voting page

--- client/src/components/display_picture.js
+++ client/src/components/display_picture.js
@@ -2,16 +2,20 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import Like from './like'
 import Dislike from './dislike'
+import imageUrl from './cutie.jpeg'
+import './display_picture.css'
 
 class Picture extends PureComponent {
   render() {
-    const { imageUrl } = this.props
+    // const { imageUrl } = this.props
 
     return (
-      <div className="dogImage">
-        <img src={imageUrl} />
-        <Like />
-        <Dislike />
+      <div className="imageBox">
+        <img className="dogImage" src={imageUrl} />
+        <div className="buttonBox">
+          <Dislike />
+          <Like />
+        </div>
       </div>
     )
   }
